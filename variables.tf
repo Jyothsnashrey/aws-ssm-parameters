37a3fb2834f568c11905140ff29827cf854e0d21
Jyothsnashrey <jyothsna.shrey@gmail.com>
2024-02-21 13:17:12 -0600

    make Grafana code

--- variables.tf
+++ variables.tf
@@ -21,7 +21,7 @@ variable "secrets" {
     "ssh.username"                              = "centos"
     "ses.username"                              = "AKIAQVXZSKBZQGEKFMZR"
     "ses.password"                              = "BNPF8gmGmHBfN8w1gOGD9vSxM/SMU1vJpStf1NNBLoh9"
-    "grafana.apikey"                            = "glsa_iPznf8KMHXevnDhDAlaxdhqbQqE5YZzF_a387a4b6"
+    "grafana.apikey"                            = "glsa_9GUmErWRoUHkhKSIRcc29PTSHkOTg7Zd_25363249"
     "dev.expense.frontend.newrelic.key"         = "NRAK-BMDPUXRA7KOAJQU0NQJ9BBZULYV"
     "dev.expense.backend.newrelic.key"          = "d9956a6780aeea098434782b799181b4FFFFNRAL"
     "elasticsearch.password"                    = "4SFV+643faD+CE9rxQV3"
